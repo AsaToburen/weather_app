8ec2eb9f05759cb58356420d72bf9ef4002d468f
Asa Toburen <atopettit@gmail.com>
2015-05-04 16:17:26 -0700

    gh-pages

--- src/app/components/forecast/forecast.controller.js
+++ src/app/components/forecast/forecast.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('weather')
-  .controller('ForecastCtrl', ['$scope', 'wunderground', function($scope, wunderground){
-  $scope.Forecast = wunderground.forecastData;
-}]);
+  .controller('ForecastCtrl', ['$scope', '$location', 'wunderground',
+    function($scope, $location, wunderground) {
+
+      $scope.Forecast = wunderground.forecastData;
+      
+    }
+  ]);
